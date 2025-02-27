ce48e3b3655dd5351975bde1b50bb77e48d19cf9
Manan Santoki <manansantoki1702@gmail.com>
2025-02-27 20:14:53 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-require('./routes')
+require('./routes') // Keep this line if it's needed for side effects in routes.js
 const express = require('express')
 const { restoreSessions } = require('./sessions')
-const { routes } = require('./routes')
+const { routes } = require('./routes') // Keep this line as well if routes are exported.
 const { maxAttachmentSize } = require('./config')
 const cors = require('cors');
 
@@ -9,10 +9,12 @@ const app = express()
 
 // Initialize Express app
 app.disable('x-powered-by')
+app.use(cors({
+    origin: '*' // Allow requests from any origin
+  }));
 app.use(express.json({ limit: maxAttachmentSize + 1000000 }))
 app.use(express.urlencoded({ limit: maxAttachmentSize + 1000000, extended: true }))
-app.use('/', routes)
-app.use(cors());
+app.use('/', routes) // Then apply your routes
 
 restoreSessions()
 
