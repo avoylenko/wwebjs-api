43729d935a25abf3ffb4fe726eeb1b71697b70d0
Manan Santoki <manansantoki1702@gmail.com>
2025-02-27 19:53:04 +0530

    update

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const { restoreSessions } = require('./sessions')
 const { routes } = require('./routes')
 const { maxAttachmentSize } = require('./config')
+const cors = require('cors');
 
 const app = express()
 
@@ -11,6 +12,7 @@ app.disable('x-powered-by')
 app.use(express.json({ limit: maxAttachmentSize + 1000000 }))
 app.use(express.urlencoded({ limit: maxAttachmentSize + 1000000, extended: true }))
 app.use('/', routes)
+app.use(cors());
 
 restoreSessions()
 
